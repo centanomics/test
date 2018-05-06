8300a4cb5a07ff39351d60f6b6feb5c6e72bf028
semyers189 <semyers@fullsail.edu>
2018-05-06 02:16:01 -0400

    made changes to the styles and innerhtml of the p and h1 tags

---
+++ main.js
@@ -0,0 +1,7 @@
+let header = document.querySelector("h1");
+
+header.innerHTML = "I have been changed";
+
+let paragraph = document.querySelector("p");
+
+paragraph.innerHTML = "I have been changed as well";
